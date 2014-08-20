4cab359e62b6ce3f0c9f68aa85adef39f33d7f05
Jessica Goulding <jgoulding2@gmail.com>
2014-08-19 21:13:24 -0600

    added notes for tomorrow game plan with Glen and methods to write

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -7,4 +7,8 @@ class ItemRepository
   def initialize(file = './data/items.csv')
     @items = RepositoryParser.load(file, class_name: Item)
   end
+
+  ##Methods needed for all, random, inspect,
+  ##find by, fina all by,
+  ##find all by name
 end
