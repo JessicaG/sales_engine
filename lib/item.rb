5e8be3304340f52ce41048c3d74d838f7e3dc769
Jessica Goulding <jgoulding2@gmail.com>
2014-08-28 08:40:24 -0600

    refactored best day

--- lib/item.rb
+++ lib/item.rb
@@ -30,12 +30,32 @@ class Item
     item_repository.find_merchant_by_merchant_id(merchant_id)
   end
 
+  # def best_day
+  #   invoice_item_dates_revenues
+  #     = invoice_items.map{|invoice_item| [invoice_item.invoice.created_at, invoice_item.total_price]}
+  #   invoice_item_revenues_grouped_by_date
+  #     = invoice_item_dates_revenues.group_by{|date_revenue| date_revenue[0]}
+  #   combined_date_revenue_array
+  #     = invoice_item_revenues_grouped_by_date.each_pair.map{|key, value| [key, value.reduce(0){|sum, ary| sum + ary[1]}]}
+  #   best_date = combined_date_revenue_array.sort_by{|ary| ary[1]}[-1][0]
+  # end
+
   def best_day
-    invoice_item_dates_revenues = invoice_items.map{|invoice_item| [invoice_item.invoice.created_at, invoice_item.total_price]}
-    invoice_item_revenues_grouped_by_date = invoice_item_dates_revenues.group_by{|date_revenue| date_revenue[0]}
-    combined_date_revenue_array = invoice_item_revenues_grouped_by_date.each_pair.map{|key, value| [key, value.reduce(0){|sum, ary| sum + ary[1]}]}
-    best_date = combined_date_revenue_array.sort_by{|ary| ary[1]}[-1][0]
+    invoices = invoice_items.map { |invoice_item| invoice_item.invoice }
+    best_invoice =
+    invoices.max_by do |invoice|
+        invoices.each do |i|
+          if invoice.id == i.id
+            invoice.amount + i.amount
+          else
+            invoice.amount
+          end
+        end
+      end
+    best_invoice.created_at
   end
+
+
 
   def revenue
     unit_price * amount_sold
